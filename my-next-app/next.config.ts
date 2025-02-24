6b1863af474b4a5ffb7dd30377ebfb5b63b24113
sirayu2525 <taiyou1685@yahoo.ne.jp>
2025-02-24 15:05:45 +0900

    add .env.local

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["source.unsplash.com"], // ✅ 外部画像のホストを許可
+  },
 };
 
 export default nextConfig;
