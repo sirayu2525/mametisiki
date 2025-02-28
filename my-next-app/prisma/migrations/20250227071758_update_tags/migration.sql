4e07e3bdf4c6890d1da299da53de167e54eb7fe5
sirayu2525 <taiyou1685@yahoo.ne.jp>
2025-02-28 14:06:36 +0900

    modify migration

--- my-next-app/prisma/migrations/20250227071758_update_tags/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `tags` column on the `Article` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Article" DROP COLUMN "tags",
-ADD COLUMN     "tags" TEXT[];
--- my-next-app/prisma/migrations/20250228050332_init/migration.sql
+++ my-next-app/prisma/migrations/20250228050332_init/migration.sql
@@ -1,15 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
-
--- DropEnum
-DROP TYPE "Role";
-
 -- CreateTable
 CREATE TABLE "Article" (
     "id" SERIAL NOT NULL,
@@ -17,7 +5,7 @@ CREATE TABLE "Article" (
     "description" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "publishedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "tags" JSONB NOT NULL,
+    "tags" TEXT[],
     "image" TEXT NOT NULL,
 
     CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
