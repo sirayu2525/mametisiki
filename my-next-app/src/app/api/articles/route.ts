15cc30610ab63cef8de0dae93a94f346f7a80b1f
sirayu2525 <taiyou1685@yahoo.ne.jp>
2025-03-03 15:17:34 +0900

    Limitのデフォルトを１２にした

--- my-next-app/src/app/api/articles/route.ts
+++ my-next-app/src/app/api/articles/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const page = parseInt(searchParams.get("page") || "1", 10);
-    const limit = parseInt(searchParams.get("limit") || "10", 10);
+    const limit = parseInt(searchParams.get("limit") || "12", 10);
     const tag = searchParams.get("tag"); // タグの取得
     const start = (page - 1) * limit;
 
