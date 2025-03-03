0fc865943849d64fc56a786e41483d21fdd46c6f
sirayu2525 <taiyou1685@yahoo.ne.jp>
2025-03-03 20:37:55 +0900

    FIX:Pageprops

--- my-next-app/src/app/articles/page.tsx
+++ my-next-app/src/app/articles/page.tsx
@@ -11,8 +11,7 @@ interface Article {
   image: string;
 }
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 async function fetchArticles(
   tag?: string,
