08f84ce11360f0bf9f9f40310d4a944592ff9a1c
sirayu2525 <taiyou1685@yahoo.ne.jp>
2025-03-05 15:26:29 +0900

    design footer

--- my-next-app/src/app/components/Footer.tsx
+++ my-next-app/src/app/components/Footer.tsx
@@ -3,18 +3,31 @@ import React from "react";
 
 export default function Footer() {
     return (
-      <footer className="w-full border-t py-6 px-8 mt-8 bg-white">
-        <div className="flex justify-between text-sm text-gray-500">
-          <div>
-            <p>© 2024 大阪公立大学新聞 Hijicho</p>
-          </div>
-          <ul className="flex space-x-4">
-            <li>Use Cases</li>
-            <li>Explore</li>
-            <li>Resources</li>
-          </ul>
-        </div>
-      </footer>
+<footer className="w-full border-t py-6 px-8 mt-8 bg-white">
+  <div className="flex flex-col md:flex-row justify-between items-center text-sm text-gray-500 gap-4">
+    <div>
+      <p>© 2024 大阪公立大学新聞 Hijicho</p>
+    </div>
+    <ul className="flex gap-6">
+      <li>
+        <a href="/" className="hover:underline">
+          Top
+        </a>
+      </li>
+      <li>
+        <a href="/articles&page=1" className="hover:underline">
+          記事一覧
+        </a>
+      </li>
+      <li>
+        <a href="/begginer" className="hover:underline">
+          新入生向け
+        </a>
+      </li>
+    </ul>
+  </div>
+</footer>
+
     );
   }
   
