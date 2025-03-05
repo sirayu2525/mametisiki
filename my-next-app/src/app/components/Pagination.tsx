718f07a299d29882afdf8a66e0f09c10dc50ba84
sirayu2525 <taiyou1685@yahoo.ne.jp>
2025-03-05 12:12:09 +0900

    FIX:Pagination

--- my-next-app/src/app/components/Pagination.tsx
+++ my-next-app/src/app/components/Pagination.tsx
@@ -22,7 +22,7 @@ export default function Pagination({ currentPage, totalPages, basePath = "/artic
 
       {/* 次のページボタン */}
       {currentPage < totalPages && (
-        <Link href={`${basePath}?page=${currentPage + 1}&tag=${tagQuery}`} className="px-4 py-2 border rounded bg-gray-200 hover:bg-gray-300">
+        <Link href={`${basePath}?page=${currentPage + 1}${tagQuery}`} className="px-4 py-2 border rounded bg-gray-200 hover:bg-gray-300">
           次へ »
         </Link>
       )}
